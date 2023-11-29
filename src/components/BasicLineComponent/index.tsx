d11029aa1bcbcac15c137f2e1c4ef442f92665ab
Tom Gardiner <tom@trevor.io>
2023-11-29 22:52:26 +0100

    validate function

--- src/components/BasicLineComponent/index.tsx
+++ src/components/BasicLineComponent/index.tsx
@@ -48,17 +48,9 @@ const chartData = (data, xAxis, yAxis) => {
 
 const msg = message => <div>{message}</div>
 
-type Props = {
-  title: string;
-  ds: Dataset;
-  xAxis: Dimension;
-  yAxis: Measure;
-  results: DataResponse;
-};
+const validate = props => {
 
-export default (props: Props) => {
-
-  console.log(props);
+  console.log(props);//TODO: cleanup
 
   const { results, ds, xAxis, yAxis, title } = props;
   const { isLoading, data, error } = results;
@@ -78,8 +70,25 @@ export default (props: Props) => {
     return msg('Unexpected error: '+error);
   }
   if(!data) {
-    return; // fixes BUG: isLoading returns false before data is ready
+    return msg('!!!'); // fixes BUG: isLoading returns false before data is ready
   }
+}
+
+type Props = {
+  title: string;
+  ds: Dataset;
+  xAxis: Dimension;
+  yAxis: Measure;
+  results: DataResponse;
+};
+
+export default (props: Props) => {
+  const { results, xAxis, yAxis, title } = props;
+  const { data } = results;
+
+  const error = validate(props);
+  if(error) 
+    return error;
 
   return <Bar options={chartOptions(title)} 
               data={chartData(data, xAxis, yAxis)} />;
