db05e9e1e4fbc79717858007c69f7dfc0efd1b7c
Tom Gardiner <tom@trevor.io>
2023-11-29 22:08:10 +0100

    added test data

--- src/test.tsx
+++ src/test.tsx
@@ -7,12 +7,28 @@ import BasicLineComponent from './components/BasicLineComponent/index';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    {/**/}
     <BasicTextComponent 
       title='Build Remarkable Analytics Experiences'
       body='The toolkit for building fast, interactive, fully-custom analytics experiences into your app'
     />
+    {/**/}
     <div style={{width: '100%', height: '300px'}}>
-      <BasicLineComponent/>
+      <BasicLineComponent
+        title='My first line chart'
+        xAxis={{ name: 'country'}}
+        yAxis={{ name: 'count'}}
+        results={{
+          isLoading: false,
+          error: null,
+          data: [
+            { country: 'US', count: 23 },
+            { country: 'UK', count: 10 },
+            { country: 'Germany', count: 5 }
+          ]
+        }}
+      />
     </div>
+    {/**/}
   </React.StrictMode>
 );
